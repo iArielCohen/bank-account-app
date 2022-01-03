e7ba8d2a38e1b87c0d5698e59ef2b1fc1a1bc58e
Ariel Cohen <arieliiox@gmail.com>
2022-01-03 16:43:26 +0200

    init

---
+++ script.js
@@ -0,0 +1,121 @@
+'use strict';
+
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const html = `
+    <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__value">${mov}</div>
+   </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin',
+      html)
+  })
+}
+displayMovements(account1.movements);
+
+const calcDisplayedBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} €`
+}
+calcDisplayedBalance(account1.movements)
+
+const newMove = [200, 450, -400, 3000, -650, -130, 70, 1300]
+const euroToUsd = 1.1;
+
+
+const movemenntUsd = newMove.map(mov => mov * euroToUsd)
+const movementsDescriptions = newMove.map((mov, i) => ` Movement ${i + 1}: You ${mov > 0 ? `deposit` : `withdrawal`} ${Math.abs(mov)}`);
+// console.log(movementsDescriptions);
+
+// console.log(movemenntUsd);
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner.toLowerCase().split(" ").map(n => n[0]).join("");
+
+  })
+}
+createUsernames(accounts);
+// console.log(accounts);
+
+const deposits = newMove.filter(mov => mov > 0);
+const withdrawal = newMove.filter(mov => mov < 0);
+// console.log(deposits);
+// console.log(withdrawal);
+
+// const balance = newMove.reduce(function (acc, cur, i) {
+//   console.log(`Iteration #${i} - ${acc} ${cur > 0 ? '+' : '-'} ${cur}`);
+//   return acc + cur
+// })
+
+// const balance = newMove.reduce((acc, cur) => acc + cur);
+
+// console.log(balance);
+
+// const max = newMove.reduce((acc, mov) => acc > mov ? acc : mov);
+// const min = newMove.reduce((acc, mov) => acc < mov ? acc : mov);
+// console.log(max);
+// console.log(min);
